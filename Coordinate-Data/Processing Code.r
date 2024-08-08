97cc60c3ee2ba969144230eb7833ee5bb4d77487
cusisom <danielcu@hawaii.edu>
2024-08-07 14:05:53 -1000

    rework1

---
+++ Coordinate-Data/Processing Code.r
@@ -0,0 +1,42 @@
+setwd("C:/Users/danny/Documents/git/Dissertation-Methods/Coordinate-Data")
+require(geomorph)
+require(SlicerMorphR)
+
+SM.log.file = "2024-08-06_10_32_50/analysis.log"
+SM.log <- parser(SM.log.file)
+head(SM.log)
+
+SM.output <- read.csv(file=paste(SM.log$output.path,
+SM.log$OutputData,
+sep = "/"))
+
+SlicerMorph.PCs <- read.table(file = paste(SM.log$output.path,
+SM.log$pcScores,
+sep="/"),
+sep=",", header = TRUE, row.names=1)
+
+PD <- SM.output[,2]
+if (!SM.log$skipped) {
+no.LM <- SM.log$no.LM
+} else {
+no.LM <- SM.log$no.LM - length(SM.log$skipped.LM)
+}
+
+##Building Array
+Coords <- arrayspecs(SM.output[,-c(1:3)],
+p=no.LM,
+k=3)
+
+dimnames(Coords) <- list(1:no.LM,
+c("x","y","z"),
+SM.log$ID)
+
+d1array.gpa <- gpagen(Coords, print.progress=FALSE)
+
+summary(d1array.gpa)
+
+Mshape.Coords<-mshape(Coords)
+head(Mshape.Coords)
+
+d1.pca<-gm.prcomp(d1array.gpa$coords)
+plot(d1.pca)
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Dissertation Methods Learning
 
+###OH SHIT things have changed
+
 I'm using this repository to consolidate, track, and build on my understanding of the methods I will need to employ throughout my dissertation. The primary aspects of my project that require new methodology are Phylogentic Reconstruction and Trait Determination, Analysis of Variance and Species Delimitation, and possible investigation in Vicariance. 
 
 ## Phylogenetic Reconstruction and Trait Determination
